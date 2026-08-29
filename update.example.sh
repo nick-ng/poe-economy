77ab8dfc70fde4550529cfbd8f772f63dc02fc10
pux <nick-ng@users.noreply.github.com>
2026-08-29 17:13:38 +1200

    feat: link to md fragments instead of having them in the main readme

--- update.example.sh
+++ update.example.sh
@@ -8,22 +8,10 @@ cd "$(dirname $0)"
 
 mkdir -p temp
 
-echo "" > README.md
-
 node ./js/lab-gems.mjs
-cat ./md-fragments/LAB_GEMS.md >> README.md
-echo "" >> README.md
-echo "" >> README.md
-
 node ./js/beasts.mjs
-cat ./md-fragments/BEASTS.md >> README.md
-echo "" >> README.md
-echo "" >> README.md
-
-cat ./md-fragments/readme-footer.md >> README.md
 
 git add ./md-fragments/
-git add README.md
 git commit -m "chore: auto update - $(date)"
 git push
 
